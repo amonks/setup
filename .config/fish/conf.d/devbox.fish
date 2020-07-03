984df246e35eee85a21518845e1892bb46c472a4
monks <a@monks.co>
2020-07-03 18:38:35 -0400

    fix devbox script

--- .config/fish/conf.d/devbox.fish
+++ .config/fish/conf.d/devbox.fish
@@ -1,7 +1,9 @@
 # Use devbox thingy lol ask shane
 if status --is-interactive
-  if ! pstree -s $fish_pid | grep -q mosh-server
-    fix-ssh
+  if which pstree 1>/dev/null
+    if ! pstree -s $fish_pid | grep -q mosh-server
+      fix-ssh
+    end
   end
 end
 
