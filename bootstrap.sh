05e58b588d26e386ae1a54218b37815bbecb1892
monks <a@monks.co>
2020-07-03 17:22:45 -0400

    error on second setup

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,11 @@
 
 cd $HOME
 
+if test -d $HOME/.cfg ; then
+  echo It looks like you have already set up.
+  exit 1
+fi
+
 git clone --bare git@github.com:amonks/setup-samsara-devbox.git $HOME/.cfg
 git --git-dir=$HOME/.cfg/ --work-tree=$HOME checkout
 
