ec3584f3de424dd84faba88d754690c482a45ace
monks <a@monks.co>
2020-07-03 21:26:13 -0400

    add insert-after function

---
+++ .config/fish/functions/insert-after.fish
@@ -0,0 +1,14 @@
+function insert-after --argument-names filename pattern insertion
+  if test -z "$insertion"
+    echo "usage: insert-after \$filename \$pattern \$insertion"
+    echo "(inserts \$insertion into \$filename after line containing \$pattern)"
+    exit 1
+  end
+
+  # by copying the file before replacing it, we retain its chmod stuff
+  cp "$filename" "$filename".tmp
+
+  awk -v text="$insertion" '/'"$pattern"'/ {print $0 RS text;next} 1' "$filename" > "$filename".tmp
+  mv "$filename".tmp "$filename"
+end
+
