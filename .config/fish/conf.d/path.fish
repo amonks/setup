7fef9b011172ac97319c3564ccb1bceae161c765
monks <a@monks.co>
2020-07-13 01:28:55 -0400

    add postgres.app path

--- .config/fish/conf.d/path.fish
+++ .config/fish/conf.d/path.fish
@@ -1,2 +1,4 @@
 set -U fish_user_paths $fish_user_paths ~/bin /snap/bin ~/.cargo/bin /opt/local/bin /opt/local/sbin
 
+set -U fish_user_paths $fish_user_paths /Applications/Postgres.app/Contents/Versions/latest/bin
+
