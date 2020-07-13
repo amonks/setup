945d743b8f013a65f0fb4e8f3e4158a007dafbf2
monks <a@monks.co>
2020-07-13 01:28:42 -0400

    autojump supports macports config path

--- .config/fish/conf.d/autojump.fish
+++ .config/fish/conf.d/autojump.fish
@@ -2,5 +2,8 @@ if status --is-interactive
   if test -f /usr/share/autojump/autojump.fish
     source /usr/share/autojump/autojump.fish
   end
+  if test -f /opt/local/share/autojump/autojump.fish
+    source /opt/local/share/autojump/autojump.fish
+  end
 end
 
