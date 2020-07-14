037bf7c4daa17d2bf09eccb168c082e1bf9ece6b
monks <a@monks.co>
2020-07-14 02:56:50 -0400

    setup-ssh-key: cross-platform

--- .config/fish/functions/setup-ssh-key.fish
+++ .config/fish/functions/setup-ssh-key.fish
@@ -6,10 +6,20 @@ function setup-ssh-key
   echo Setting up ssh key
 
   ssh-keygen -t rsa -b 4096 -C "a@monks.co"
-  eval "(ssh-agent -s)"
-  ssh-add -K ~/.ssh/id_rsa
-  open "https://github.com/settings/ssh/new"
-  show-text ~/.ssh/id_rsa.pub<
-  wait_for_enter
+  eval (ssh-agent -c)
+
+  if test (uname) = Darwin
+    ssh-add -K ~/.ssh/id_rsa
+    show-text ~/.ssh/id_rsa.pub
+    open "https://github.com/settings/ssh/new"
+  else if test (uname) = Linux
+    ssh-add ~/.ssh/id_rsa
+    show-text ~/.ssh/id_rsa.pub
+    echo
+    echo set up the key here:
+    echo https://github.com/settings/ssh/new
+  end
+
+  wait-for-enter
 end
 
