530eeadf70a14a256941963a0cfcf8a9bb7db308
monks <a@monks.co>
2020-08-12 16:04:59 -0400

    fish: support autojump from brew

--- .config/fish/conf.d/autojump.fish
+++ .config/fish/conf.d/autojump.fish
@@ -1,9 +1,10 @@
 if status --is-interactive
   if test -f /usr/share/autojump/autojump.fish
     source /usr/share/autojump/autojump.fish
-  end
-  if test -f /opt/local/share/autojump/autojump.fish
+  else if test -f /opt/local/share/autojump/autojump.fish
     source /opt/local/share/autojump/autojump.fish
+  else if test -f /usr/local/share/autojump/autojump.fish
+    source /usr/local/share/autojump/autojump.fish
   end
 end
 
