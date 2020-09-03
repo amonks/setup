daecf2487c1d0b455112775600efcbd40d688621
monks <a@monks.co>
2020-09-02 22:45:40 -0400

    speed up fish path conf

--- .config/fish/conf.d/path.fish
+++ .config/fish/conf.d/path.fish
@@ -1,13 +1,2 @@
-function add-to-path --argument-names path
-  if test -d $path
-    set -U fish_user_paths $fish_user_paths $path
-  end
-end
-
-add-to-path ~/bin
-add-to-path /snap/bin
-add-to-path ~/.cargo/bin
-add-to-path /opt/local/bin
-add-to-path /opt/local/sbin
-add-to-path /Applications/Postgres.app/Contents/Versions/latest/bin
+set -U fish_user_paths $fish_user_paths ~/bin /snap/bin ~/.cargo/bin /opt/local/bin /opt/local/sbin /Applications/Postgres.app/Contents/Versions/latest/bin
 
