02cff635b1f8b41dcfdf917458310666c1781c67
monks <a@monks.co>
2020-11-14 20:26:39 -0500

    fish: stop making fish_user_paths infinitely large
    this slowed down opening new shells
    
    https://github.com/fish-shell/fish-shell/issues/5834#issuecomment-485070486

--- .config/fish/conf.d/path.fish
+++ .config/fish/conf.d/path.fish
@@ -1,2 +1,2 @@
-set -U fish_user_paths $fish_user_paths ~/bin /snap/bin ~/.cargo/bin /opt/local/bin /opt/local/sbin /Applications/Postgres.app/Contents/Versions/latest/bin
+set -U fish_user_paths ~/bin /snap/bin ~/.cargo/bin /opt/local/bin /opt/local/sbin /Applications/Postgres.app/Contents/Versions/latest/bin
 
