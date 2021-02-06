36b8a19a983016ce241df4b1431b89a7c9de2519
monks <a@monks.co>
2021-02-06 14:22:34 -0500

    fish: add go path to path

--- .config/fish/conf.d/path.fish
+++ .config/fish/conf.d/path.fish
@@ -1,2 +1,2 @@
-set -U fish_user_paths ~/bin /usr/local/opt/ruby/bin /usr/local/lib/ruby/gems/2.7.0/bin /snap/bin ~/.cargo/bin /opt/local/bin /opt/local/sbin /Applications/Postgres.app/Contents/Versions/latest/bin ~/.fzf/bin
+set -U fish_user_paths ~/bin /usr/local/opt/ruby/bin /usr/local/lib/ruby/gems/2.7.0/bin /snap/bin ~/.cargo/bin /opt/local/bin /opt/local/sbin /Applications/Postgres.app/Contents/Versions/latest/bin ~/.fzf/bin /usr/local/go/bin
 
