e14e810d474b47939870f995f23cab8446117b61
monks <a@monks.co>
2021-02-16 22:30:30 -0500

    fish: allow disabling tmux per-machine

--- .config/fish/conf.d/tmux.fish
+++ .config/fish/conf.d/tmux.fish
@@ -2,8 +2,8 @@
 if test -n "$TERM"
   if status --is-login
     if is-installed tmux
-      if test "$TERM" != "screen"; and test -z "$TMUX"
-	exec tmux new-session -A -s main
+      if test "$TERM" != "screen"; and test -z "$TMUX"; and test "$use_tmux" != "false"
+        exec tmux new-session -A -s main
       end
     end
   end
