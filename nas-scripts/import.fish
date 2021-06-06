2f9d9ae54058d8b2f684382810f0d84ed47dd6ed
monks <a@monks.co>
2021-06-06 03:22:40 -0400

    nas-scripts: support spaces in album names :facepalm:

--- nas-scripts/import.fish
+++ nas-scripts/import.fish
@@ -4,7 +4,7 @@ set list_file (mktemp)
 exa --sort newest --reverse /mypool/data/mirror/whatbox/files/flac > $list_file
 nvim $list_file
 for album in (cat $list_file)
-	beet import /mypool/data/mirror/whatbox/files/flac/$album
+	beet import "/mypool/data/mirror/whatbox/files/flac/$album"
 end
 rm $list_file
 
