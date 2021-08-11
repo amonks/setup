d8aabe6356f95a92af71d884fa09585d209758f8
monks <a@monks.co>
2021-08-11 13:41:32 -0400

    ssh: move devbox host into Include file
    this way, set-devbox-ip won't create diffs in the tracked .ssh/config file

--- .config/fish/functions/set-devbox-ip.fish
+++ .config/fish/functions/set-devbox-ip.fish
@@ -1,5 +1,15 @@
+set empty_devbox_config "
+Host devbox
+  # DEVBOX_HOST
+  Hostname 0.0.0.0
+"
+
 function set-devbox-ip
-  delete-after ~/.ssh/config DEVBOX_HOST
-  insert-after ~/.ssh/config DEVBOX_HOST (get-devbox-ip)
+  if ! test -f ~/.ssh/devbox.config
+    echo "$empty_devbox_config" > ~/.ssh/devbox.config
+  end
+
+  delete-after ~/.ssh/devbox.config DEVBOX_HOST
+  insert-after ~/.ssh/devbox.config DEVBOX_HOST (get-devbox-ip)
 end
 
