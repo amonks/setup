3687d07e5880cdbbb059e5f285c514b1be72f74f
monks <a@monks.co>
2021-10-10 03:51:07 -0500

    fish: add emacs stuff to path

--- .config/fish/conf.d/path.fish
+++ .config/fish/conf.d/path.fish
@@ -1,2 +1,1 @@
-set -U fish_user_paths ~/bin /usr/local/opt/ruby/bin /usr/local/lib/ruby/gems/2.7.0/bin /snap/bin ~/.cargo/bin /opt/local/bin /opt/local/sbin /Applications/Postgres.app/Contents/Versions/latest/bin ~/.fzf/bin /usr/local/go/bin ~/.deno/bin/ ~/android-platform-tools ~/go/bin
-
+set -U fish_user_paths ~/bin /usr/local/opt/ruby/bin /usr/local/lib/ruby/gems/2.7.0/bin /snap/bin ~/.cargo/bin /opt/local/bin /opt/local/sbin /Applications/Postgres.app/Contents/Versions/latest/bin ~/.fzf/bin /usr/local/go/bin ~/.deno/bin/ ~/android-platform-tools ~/go/bin /Applications/MacPorts/Emacs.app/Contents/MacOS ~/.emacs.d/bin
