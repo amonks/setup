ff8fdb64a60b738c1b37d202a7d588842f9b66b8
monks <a@monks.co>
2021-10-29 05:10:42 -0400

    fish setup-ssh-key: ed25519, add to authorized_keys

--- .config/fish/functions/setup-ssh-key.fish
+++ .config/fish/functions/setup-ssh-key.fish
@@ -1,24 +1,35 @@
 function setup-ssh-key
-  if test -f ~/.ssh/id_rsa.pub
+  if test -f ~/.ssh/id_ed25519.pub
     return
   end
 
-  echo Setting up ssh key
+  if test -z $machine_name || test -z $machine_user
+    echo 'Set $machine_name and $machine_user in ~/locals.fish'
+    return 1
+  end
 
-  ssh-keygen -t rsa -b 4096 -C "a@monks.co"
+  set --local id "($machine_user)@($machine_name)"
+  set --local algo ed25519
+  set --local pem_file ~/.ssh/id_$algo
+  set --local pub_file ~/.ssh/id_$algo.pub
+
+  echo "Setting up ssh key for $id"
+
+  ssh-keygen -t $algo -C "$id"
   eval (ssh-agent -c)
 
   if test (uname) = Darwin
-    ssh-add -K ~/.ssh/id_rsa
-    show-text ~/.ssh/id_rsa.pub
-    open "https://github.com/settings/ssh/new"
+    ssh-add -K $pem_file
   else if test (uname) = Linux
-    ssh-add ~/.ssh/id_rsa
-    show-text ~/.ssh/id_rsa.pub
-    echo
-    echo set up the key here:
-    echo https://github.com/settings/ssh/new
+    ssh-add $pem_file
   end
+
+  echo (cat $pub_file) >> ~/.ssh/authorized_keys
+
+  show-text $pub_file
+  echo
+  echo set up the key here:
+  echo https://github.com/settings/ssh/new
 
   wait-for-enter
 end
