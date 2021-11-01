b8b52df05eed3760bc702c8ae1e52d4d62745804
monks <a@monks.co>
2021-11-01 16:37:22 -0500

    fish: remove extra parens in setup-ssh-key

--- .config/fish/functions/setup-ssh-key.fish
+++ .config/fish/functions/setup-ssh-key.fish
@@ -1,36 +1,35 @@
 function setup-ssh-key
-  if test -f ~/.ssh/id_ed25519.pub
-    return
-  end
+    if test -f ~/.ssh/id_ed25519.pub
+        return
+    end
 
-  if test -z $machine_name || test -z $machine_user
-    echo 'Set $machine_name and $machine_user in ~/locals.fish'
-    return 1
-  end
+    if test -z $machine_name || test -z $machine_user
+        echo 'Set $machine_name and $machine_user in ~/locals.fish'
+        return 1
+    end
 
-  set --local id "($machine_user)@($machine_name)"
-  set --local algo ed25519
-  set --local pem_file ~/.ssh/id_$algo
-  set --local pub_file ~/.ssh/id_$algo.pub
+    set --local id "$machine_user@$machine_name"
+    set --local algo ed25519
+    set --local pem_file ~/.ssh/id_$algo
+    set --local pub_file ~/.ssh/id_$algo.pub
 
-  echo "Setting up ssh key for $id"
+    echo "Setting up ssh key for $id"
 
-  ssh-keygen -t $algo -C "$id"
-  eval (ssh-agent -c)
+    ssh-keygen -t $algo -C "$id"
+    eval (ssh-agent -c)
 
-  if test (uname) = Darwin
-    ssh-add -K $pem_file
-  else if test (uname) = Linux
-    ssh-add $pem_file
-  end
+    if test (uname) = Darwin
+        ssh-add -K $pem_file
+    else if test (uname) = Linux
+        ssh-add $pem_file
+    end
 
-  echo (cat $pub_file) >> ~/.ssh/authorized_keys
+    echo (cat $pub_file) >>~/.ssh/authorized_keys
 
-  show-text $pub_file
-  echo
-  echo set up the key here:
-  echo https://github.com/settings/ssh/new
+    show-text $pub_file
+    echo
+    echo set up the key here:
+    echo https://github.com/settings/ssh/new
 
-  wait-for-enter
+    wait-for-enter
 end
-
