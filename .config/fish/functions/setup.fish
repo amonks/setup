71e5d3aa28fd1719f514525e423f06423685e8d1
monks <a@monks.co>
2021-11-12 00:39:32 -0600

    setup: install quicklisp with lisp env

--- .config/fish/functions/setup.fish
+++ .config/fish/functions/setup.fish
@@ -70,6 +70,13 @@ function setup
 
     if has-setup-option setup_lisp_environment
         install-package --name sbcl
+        if ! test -d ~/.quicklisp
+            curl -o /tmp/ql.lisp http://beta.quicklisp.org/quicklisp.lisp
+            sbcl --no-sysinit --no-userinit --load /tmp/ql.lisp \
+                --eval '(quicklisp-quickstart:install :path "~/.quicklisp")' \
+                --eval '(ql:add-to-init-file)' \
+                --quit
+        end
     end
 
     if has-setup-option setup_racket_environment
