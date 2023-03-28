57b6c81147aff1ef7e86d38ba58ebab25f05f143
monks <a@monks.co>
2023-03-27 20:30:20 -0400

    fish bell: support argument for number of bells

--- .config/fish/functions/bell.fish
+++ .config/fish/functions/bell.fish
@@ -1,4 +1,13 @@
-function bell
+function bell --argument-names count
   echo -n \a
+
+  if test -n "$count"
+    if test $count -gt 1
+      for i in (seq 2 $count)
+        sleep 0.1
+        echo -n \a
+      end
+    end
+  end
 end
 
