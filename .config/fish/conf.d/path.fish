1bf80c8f437e72a20e42546ef878bed556eb7d99
monks <a@monks.co>
2023-05-10 08:20:15 -0500

    fish PATH: add python3.9 bin

--- .config/fish/conf.d/path.fish
+++ .config/fish/conf.d/path.fish
@@ -1,4 +1,4 @@
 debug-fish-init start (status -f)
-  set -U fish_user_paths ~/bin /usr/local/opt/ruby/bin /usr/local/lib/ruby/gems/2.7.0/bin /snap/bin ~/.cargo/bin /opt/local/bin /opt/local/sbin /Applications/Postgres.app/Contents/Versions/latest/bin ~/.fzf/bin /usr/local/go/bin ~/.deno/bin/ ~/android-platform-tools ~/go/bin ~/.emacs.d/bin /Applications/Racket/bin ~/Library/Python/3.8/bin
+  set -U fish_user_paths ~/bin /usr/local/opt/ruby/bin /usr/local/lib/ruby/gems/2.7.0/bin /snap/bin ~/.cargo/bin /opt/local/bin /opt/local/sbin /Applications/Postgres.app/Contents/Versions/latest/bin ~/.fzf/bin /usr/local/go/bin ~/.deno/bin/ ~/android-platform-tools ~/go/bin ~/.emacs.d/bin /Applications/Racket/bin ~/Library/Python/3.8/bin ~/Library/Python/3.9/bin
 debug-fish-init end (status -f)
 
