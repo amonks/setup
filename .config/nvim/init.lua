589adbb99da0413c7ff2ce8506a3d888b12d16b2
monks <a@monks.co>
2023-06-07 20:38:49 -0500

    vim: better fzf integration

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -198,17 +198,21 @@ require('nvim-treesitter.configs').setup {
 
 
 
-
--- grep
-vim.api.nvim_create_user_command("Rg", function(p) 
+-- live grep
+nmap("<C-f>", function()
   local fzf_run = vim.fn["fzf#run"]
   local fzf_wrp = vim.fn["fzf#wrap"]
+  rg_prefix="rg --column --line-number --no-heading --color=always --smart-case"
   fzf_run(fzf_wrp({
-    source = "rg --column --line-number --no-heading --smart-case " .. vim.fn.shellescape(p.args),
-    options = {},
-    sinklist = function(lines)
-      local item = lines[1]
-
+    source = "echo ''",
+    options = {
+      "--bind", "start:reload:"..rg_prefix.." ''",
+      "--bind", "change:reload:"..rg_prefix.." {q} || true",
+      "--ansi", "--disabled",
+      "--layout=reverse",
+    },
+    sink = function(item)
+      print("hello", item)
       local firstColonIndex, _ = string.find(item, ":")
       local filepath = string.sub(item, 1, firstColonIndex-1)
 
@@ -218,14 +222,22 @@ vim.api.nvim_create_user_command("Rg", function(p)
       vim.cmd("e +"..lineno.." "..filepath)
     end,
   }))
-end, {nargs="+"})
+end)
 
 -- ctrlp
-nmap("<C-p>", function() 
-  local file = vim.api.nvim_call_function("fzf#run", {{
-    source = "fd --type=f --hidden --ignore --exclude=.git",
-    sink = "e",
-  }})
+nmap("<C-p>", function()
+  local fzf_run = vim.fn["fzf#run"]
+  local fzf_wrp = vim.fn["fzf#wrap"]
+  fd = "fd --type=f --hidden --ignore --exclude=.git"
+  fzf_run(fzf_wrp({
+    source = "echo ''",
+    options = {
+      "--bind", "start:reload:"..fd,
+      "--ansi",
+      "--layout=reverse",
+    },
+    sink = "e"
+  }))
 end)
 
 
