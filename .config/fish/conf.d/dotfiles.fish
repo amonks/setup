7ef935aa8390030cd06d5ad30bc5df0d67e958ab
monks <a@monks.co>
2023-06-18 02:01:21 -0500

    fish: stop setting git config in conf.d

--- .config/fish/conf.d/dotfiles.fish
+++ .config/fish/conf.d/dotfiles.fish
@@ -2,7 +2,6 @@
 debug-fish-init start (status -f)
   if test -d "$HOME/.cfg"
     alias config='git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
-    config config --local status.showUntrackedFiles no
   end
 debug-fish-init end (status -f)
 
