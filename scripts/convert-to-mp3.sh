cb614c6a5a21915b0f46cebfb6ee8fadf4e3d23b
monks <a@monks.co>
2023-09-05 16:24:12 -0400

    beet: add v0 and 320 conversions

--- scripts/convert-to-mp3.sh
+++ scripts/convert-to-mp3.sh
@@ -1,3 +1,19 @@
 #!/bin/sh
-ffmpeg -i "$1" -f wav - | lame -V 0 --noreplaygain - "$2"
+
+format="$1"
+src="$2"
+dest="$3"
+
+# echo "format: $format"
+# echo "src: $src"
+# echo "dest: $dest"
+
+if test "$format" = "v0" ; then
+  ffmpeg -i "$src" -f wav - | lame -V 0 --noreplaygain - "$dest"
+elif test "$format" = "320" ; then
+  ffmpeg -i "$src" -f wav - | lame --cbr -b 320 --noreplaygain - "$dest"
+else
+  echo "unexpected format: '$format'"
+  exit 1
+fi
 
