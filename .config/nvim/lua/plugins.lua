59bc5f867413fdae24b37ac37b34c5a26353f0f7
monks <a@monks.co>
2024-03-31 15:19:44 -0500

    nvim: s/packer/lazy.nvim

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,41 +1,44 @@
-vim.cmd([[
-    " install packer if necessary
-    let clone_dir = stdpath('config') . '/pack/packer/start/packer.nvim'
-    if !filereadable(clone_dir)
-        silent execute '!git clone --depth 1 https://github.com/wbthomason/packer.nvim ' . clone_dir
-    endif
-    unlet clone_dir
-]])
+-- install lazy if necessary
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
 
-require('packer').startup(function(use)
-    use 'lewis6991/gitsigns.nvim'
-
-    use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' } -- fast syntax highlighting
-
-    use 'christoomey/vim-tmux-navigator'      -- ctrl+j,k,l,m across tmux and vim panes (doesn't support nesting)
-    use 'dhruvasagar/vim-table-mode'          -- markdown tables; use :TableModeToggle
-    use 'easymotion/vim-easymotion'           -- type, eg, ,,j
-    use 'google/vim-searchindex'              -- show "N of M"
-    use 'jose-elias-alvarez/null-ls.nvim'     -- uses nvim's lsp integration as a hook to add a bunch of non-lsp tools
-    use 'junegunn/fzf'                        -- ctrlp, search -- telescope seems cool but the implementation is insane
-    use 'masukomi/vim-markdown-folding'       -- makes markdown headers foldable
-    use 'mbbill/undotree'                     -- UndotreeToggle
-    use 'neovim/nvim-lspconfig'               -- seems required for using builtin lsp
-    use 'nvim-lua/plenary.nvim'               -- dependency of many lua plugins
-    use 'stefandtw/quickfix-reflector.vim'    -- find-and-replace
-    use 'vrischmann/tree-sitter-templ'        -- highlighting for go-templ
+require('lazy').setup({
+    'christoomey/vim-tmux-navigator',     -- ctrl+j,k,l,m across tmux and vim panes (doesn't support nesting)
+    'dhruvasagar/vim-table-mode',         -- markdown tables; use :TableModeToggle
+    'easymotion/vim-easymotion',          -- type, eg, ,,j
+    'google/vim-searchindex',             -- show "N of M"
+    'jose-elias-alvarez/null-ls.nvim',    -- uses nvim's lsp integration as a hook to add a bunch of non-lsp tools
+    'junegunn/fzf',                       -- ctrlp, search -- telescope seems cool but the implementation is insane
+    'lewis6991/gitsigns.nvim',            -- gitgutter
+    'masukomi/vim-markdown-folding',      -- makes markdown headers foldable
+    'mbbill/undotree',                    -- UndotreeToggle
+    'neovim/nvim-lspconfig',              -- seems required for using builtin lsp
+    'nvim-lua/plenary.nvim',              -- dependency of many lua plugins
+    'nvim-treesitter/nvim-treesitter',    -- fast syntax highlighting
+    'stefandtw/quickfix-reflector.vim',   -- find-and-replace
+    'vrischmann/tree-sitter-templ',       -- highlighting for go-templ
 
     -- tpope section (very based)
-    use 'tpope/vim-abolish'                   -- :%Subvert/facilit{y,ies}/building{,s}/g, crs(nake)
-    use 'tpope/vim-commentary'                -- gcc
-    use 'tpope/vim-eunuch'                    -- :Rename (also renames buffer), :SudoWrite, :Mkdir
+    'tpope/vim-abolish',                  -- :%Subvert/facilit{y,ies}/building{,s}/g, crs(nake)
+    'tpope/vim-commentary',               -- gcc
+    'tpope/vim-eunuch',                   -- :Rename (also renames buffer), :SudoWrite, :Mkdir
                                               -- also, redetect filetype and chmod+x after writing #! line
-    use 'tpope/vim-fireplace'                 -- clojure repl
-    use 'tpope/vim-fugitive'                  -- :Git blame
-    use 'tpope/vim-repeat'                    -- make . repeat more things
-    use 'tpope/vim-rsi'                       -- use bash-style insert bindings in commandline
-    use 'tpope/vim-sleuth'                    -- automatically detect indent
-    use 'tpope/vim-surround'                  -- ysiW"
-    use 'tpope/vim-vinegar'                   -- press - to go to netrw
-end)
+    'tpope/vim-fireplace',                -- clojure repl
+    'tpope/vim-fugitive',                 -- :Git blame
+    'tpope/vim-repeat',                   -- make . repeat more things
+    'tpope/vim-rsi',                      -- use bash-style insert bindings in commandline
+    'tpope/vim-sleuth',                   -- automatically detect indent
+    'tpope/vim-surround',                 -- ysiW"
+    'tpope/vim-vinegar',                  -- press - to go to netrw
+})
 
