dc058c650a3b584ada1afe34514ab0e91a148345
monks <a@monks.co>
2024-06-05 05:37:25 -0500

    fan-control

--- scripts/fan-control.fish
+++ scripts/fan-control.fish
@@ -8,10 +8,15 @@ end
 
 # Set fan mode: raw 0x30 0x45 0x01 [x]
 # [x] Modes:      CPU Zone  Peripheral Zone
-#                 Target    Target 
+#                 Target    Target
 # -------------   --------  ---------------
 # 00  Standard    50%      50%
 # 01  Full        100%     100%
 # 02  Optimal     30%      30%
 # 04  Heavy I/O   50%      75%
+
+#set fan mode to "full"
+sudo ipmitool raw 0x30 0x45 0x01 0x01
+
+# set fan mode to optimal
 sudo ipmitool raw 0x30 0x45 0x01 0x02
