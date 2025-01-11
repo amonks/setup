a2c0546fa575c62c548833f2fd57f2ba92e68884
monks <a@monks.co>
2025-01-11 14:11:41 -0600

    delete stuff; tmux-askpass

--- .config/fish/conf.d/tmux.fish
+++ .config/fish/conf.d/tmux.fish
@@ -2,7 +2,7 @@ debug-fish-init start (status -f)
   if has-setup-option use_tmux; and is-installed tmux
     if test -n "$TERM"; and status --is-login
       if test "$TERM" != screen; and test -z "$TMUX"; and test "$use_tmux" != false
-        exec tmux new-session -A -s main
+        SUDO_ASKPASS=$HOME/bin/tmux-askpass exec tmux new-session -A -s main
       end
     end
   end
