e29aed89328e8f23389cae1dd9702c2646c8c339
monks <a@monks.co>
2025-01-23 13:44:15 -0600

    model stuff; backupd.toml

--- .config/nvim/lua/prompts.lua
+++ .config/nvim/lua/prompts.lua
@@ -4,19 +4,32 @@ return function()
   local openai_provider = require('model.providers.openai')
 
   require('model.providers.openai').initialize({
-    model = 'gpt-4o'
+    model = 'gpt-o1'
   })
 
-  build_model = function(model_builders)
-    model = {}
-    for i,builder in ipairs(model_builders) do
-      builder(model)
-    end
-    return model
+  function clone(obj, seen)
+    if type(obj) ~= 'table' then return obj end
+    if seen and seen[obj] then return seen[obj] end
+    local s = seen or {}
+    local res = setmetatable({}, getmetatable(obj))
+    s[obj] = res
+    for k, v in pairs(obj) do res[clone(k, s)] = clone(v, s) end
+    return res
   end
 
-  build_prompter = function(prompt_builders)
+  function builder(fn)
     return function(opt)
+      dup = clone(opt)
+      fn(dup)
+      return dup
+    end
+  end
+
+
+  -- Prompt Builders
+
+  build_prompter = function(prompt_builders)
+    return builder(function(opt)
       opt.builder = function(input, context)
         msgs = {}
         for i,builder in ipairs(prompt_builders) do
@@ -24,7 +37,7 @@ return function()
         end
         return { messages = msgs }
       end
-    end
+    end)
   end
 
   system_prompt = function(content)
@@ -37,20 +50,33 @@ return function()
     table.insert(msgs, { role = 'user', content = input })
   end
 
-  output_append = function(opt) opt.mode = mode.APPEND end
-  output_replace = function(opt) opt.mode = mode.REPLACE end
-  output_buffer = function(opt) opt.mode = mode.BUFFER end
-  output_insert = function(opt) opt.mode = mode.INSERT end
-  output_insert_or_replace = function(opt) opt.mode = mode.INSERT_OR_REPLACE end
+  go_prompt = system_prompt("You are an AI for programming in the Go language. Output only valid Go code, do not output markdown code blocks.")
 
-  openai = function(opt)
+
+  -- Model Builders
+
+  build_model = function(model_builders)
+    model = {}
+    for i,builder in ipairs(model_builders) do
+      model = builder(model)
+    end
+    return model
+  end
+
+  output_append            = builder(function(opt) opt.mode = mode.APPEND end)
+  output_replace           = builder(function(opt) opt.mode = mode.REPLACE end)
+  output_buffer            = builder(function(opt) opt.mode = mode.BUFFER end)
+  output_insert            = builder(function(opt) opt.mode = mode.INSERT end)
+  output_insert_or_replace = builder(function(opt) opt.mode = mode.INSERT_OR_REPLACE end)
+
+  openai = builder(function(opt)
     opt.provider = openai_provider
     opt.params = {
       model = "gpt-4o",
     }
-  end
+  end)
 
-  claude = function(opt)
+  claude = builder(function(opt)
     opt.provider = anthropic_provider
     opt.params = {
       model = "claud-3-5-sonnet-latest",
@@ -65,35 +91,33 @@ return function()
       end
       return built
     end
-  end
+  end)
 
-  go_prompt = system_prompt("You are an AI for programming in the Go language. Output only valid Go code, do not output markdown code blocks.")
-
-  prompts = {}
-  prompts.gpt_append = build_model({
+  library = {}
+  library.gpt_append = build_model({
     openai,
     output_append,
     build_prompter({
-      go_prompt,
+      -- go_prompt,
       selected_input,
     })
   })
-  prompts.gpt_buf = output_buffer(prompts.gpt_append)
-  prompts.gpt_rep = output_replace(prompts.gpt_append)
+  library.gpt_buf = output_buffer(library.gpt_append)
+  library.gpt_rep = output_replace(library.gpt_append)
 
-  prompts.claude_append = build_model({
+  library.claude_append = build_model({
     output_append,
     build_prompter({
-      go_prompt,
+      -- go_prompt,
       selected_input,
     }),
     claude,
   })
-  prompts.claude_buf = output_buffer(prompts.claude_append)
-  prompts.claude_rep = output_replace(prompts.claude_append)
+  library.claude_buf = output_buffer(library.claude_append)
+  library.claude_rep = output_replace(library.claude_append)
 
   require('model').setup({
-    default_prompt = prompts.claude_append,
-    prompts = prompts,
+    default_prompt = library.claude_append,
+    prompts = library,
   })
 end
