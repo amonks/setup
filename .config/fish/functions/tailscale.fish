c34591cedfe29b5aa56aef398506781084df88ea
monks <a@monks.co>
2025-01-24 14:20:57 -0600

    add tailscale fn

---
+++ .config/fish/functions/tailscale.fish
@@ -0,0 +1,12 @@
+function tailscale
+  if is-installed tailscale
+    tailscale $argv
+  else if test -f /Applications/Tailscale.app/Contents/MacOS/Tailscale
+    /Applications/Tailscale.app/Contents/MacOS/Tailscale $argv
+  else
+    echo "tailscale not installed" >&2
+    return 1
+  end
+end
+
+
