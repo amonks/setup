967df3d887ec1d27421bb428ea04e64b325843e8
monks <a@monks.co>
2025-02-20 15:50:45 -0600

    stuff

--- scripts/beet-import.fish
+++ scripts/beet-import.fish
@@ -1,5 +1,7 @@
 #!/usr/bin/env fish
 
+set flac_path /usr/home/ajm/mnt/whatbox/files/flac
+
 if test "$machine_name" != "thor"
 	echo "must run on thor"
 	exit 1
@@ -10,8 +12,6 @@ set import_flags "-l" "beet-import.log"
 if contains -- --quiet $argv
 	set import_flags "-ql" "beet-import.log"
 end
-
-set flac_path /usr/home/ajm/mnt/whatbox/files/flac
 
 set list_file (mktemp)
 exa --sort newest --reverse $flac_path > $list_file
@@ -27,9 +27,25 @@ if test (count $albums) -eq "0"
 	exit 0
 end
 
-echo importing (count $albums) albums.
-beet import $import_flags $albums
+set total_albums (count $albums)
+set batch_size 10
+set current_index 1
 
-and echo done importing. converting to mp3.
-and ~/scripts/beet-convert-mp3s.fish
+echo "Importing $total_albums albums in batches of $batch_size..."
+
+while test $current_index -le $total_albums
+	set end_index (math min $current_index + $batch_size - 1, $total_albums)
+	set current_batch $albums[$current_index..$end_index]
+	
+	echo "Processing batch $current_index to $end_index of $total_albums"
+	beet import $import_flags $current_batch
+	or exit 1
+	
+	set current_index (math $end_index + 1)
+end
+
+if ! contains -- --no-convert $argv
+	echo "Done importing. Converting to mp3."
+	~/scripts/beet-convert-mp3s.fish
+end
 
