565dd63445f88493de09836d30f1e2f75bf26c35
monks <a@monks.co>
2025-09-11 01:34:26 +0000

    fish: don't require atuin

--- .config/fish/conf.d/atuin.fish
+++ .config/fish/conf.d/atuin.fish
@@ -1,5 +1,5 @@
 debug-fish-init start (status -f)
-  if status --is-interactive
+  if status --is-interactive && is-installed atuin
     atuin init fish | source
   end
 debug-fish-init end (status -f)
