6d6e48d93d6ab7ec249f222a2a19a350f4dce99b
monks <a@monks.co>
2026-04-11 03:34:01 -0500

    setup: update rustc

--- .config/fish/functions/setup.fish
+++ .config/fish/functions/setup.fish
@@ -69,6 +69,9 @@ function setup
         if ! is-installed rustc
             echo "Installing rust"
             curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+        else
+            echo "Updating rust"
+            rustup update
         end
 
         if ! is-installed atuin
