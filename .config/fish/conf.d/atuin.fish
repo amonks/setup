3bcb542ff4a2ac2f41b51dee9708a61093aab552
monks <a@monks.co>
2026-04-12 00:13:09 -0500

    zellij: atuin session per pane

--- .config/fish/conf.d/atuin.fish
+++ .config/fish/conf.d/atuin.fish
@@ -1,5 +1,13 @@
 debug-fish-init start (status -f)
   if is-installed atuin && status --is-interactive
+    # Each Zellij pane should get its own atuin session so that
+    # up-arrow (filter_mode_shell_up_key_binding = "session") only
+    # shows commands from this pane. Without this, all panes inherit
+    # the same ATUIN_SESSION from the parent environment.
+    if set -q ZELLIJ_PANE_ID; and test "$ATUIN_ZELLIJ_PANE_ID" != "$ZELLIJ_PANE_ID"
+      set -gx ATUIN_ZELLIJ_PANE_ID $ZELLIJ_PANE_ID
+      set -e ATUIN_SESSION
+    end
     atuin init fish | source
   end
 debug-fish-init end (status -f)
