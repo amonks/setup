c85f1e1e2b7e6240a9b74919613d9ba529ff4509
monks <a@monks.co>
2026-05-11 16:10:50 -0500

    zellij: stronger test

--- .config/fish/conf.d/zellij.fish
+++ .config/fish/conf.d/zellij.fish
@@ -1,10 +1,9 @@
 debug-fish-init start (status -f)
   if has-setup-option use_zellij; and is-installed zellij
     if test -n "$TERM"; and status --is-login
-      if test "$TERM" != screen; and test -z "$ZELLIJ"; and test "$use_zellij" != false
+      if not string match -q 'screen*' "$TERM"; and not string match -q 'tmux*' "$TERM"; and test -z "$ZELLIJ"; and test "$use_zellij" != false
         exec zellij attach -c main
       end
     end
   end
 debug-fish-init end (status -f)
-
